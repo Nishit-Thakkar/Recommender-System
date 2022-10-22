c0f72e72956aa04ac781b4f3fad6e168bb814b88
Jannish Gaming <jvt3112@yahoo.com>
2022-10-22 10:47:34 +0530

    my second commit

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def fetch_poster(movie_id):
     data = requests.get(url)
     data = data.json()
     poster_path = data['poster_path']
-    full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
+    full_path = "https://image.tmdb.org/t/p/w500/" + poster_path    
     return full_path
 
 def recommend(movie):
@@ -16,13 +16,15 @@ def recommend(movie):
     distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
     recommended_movie_names = []
     recommended_movie_posters = []
+    recommended_movie_homeage = []
     for i in distances[1:6]:
         # fetch the movie poster
         movie_id = movies.iloc[i[0]].movie_id
         recommended_movie_posters.append(fetch_poster(movie_id))
         recommended_movie_names.append(movies.iloc[i[0]].title)
+        recommended_movie_homeage.append(movies.iloc[i[0]].homepage)
 
-    return recommended_movie_names,recommended_movie_posters
+    return recommended_movie_names,recommended_movie_posters,recommended_movie_homeage
 
 
 st.header('Movie Recommender System By Nishit Thakkar')
@@ -37,24 +39,50 @@ selected_movie = st.selectbox(
 )
 
 if st.button('Show Recommendation'):
-    recommended_movie_names,recommended_movie_posters = recommend(selected_movie)
+    recommended_movie_names,recommended_movie_posters,recommended_movie_homeage = recommend(selected_movie)
     col1, col2, col3, col4, col5 = st.columns(5)
     with col1:
         st.text(recommended_movie_names[0])
         st.image(recommended_movie_posters[0])
+        url=recommended_movie_homeage[0]
+        if pd.isna(url):
+            st.markdown("Movie Link is not available")
+        else:
+            st.markdown("Movie Link [link](%s)" % url)
+        
     with col2:
         st.text(recommended_movie_names[1])
         st.image(recommended_movie_posters[1])
+        url=recommended_movie_homeage[1]
+        if pd.isna(url):
+            st.markdown("Movie Link is not available")
+        else:
+            st.markdown("Movie Link [link](%s)" % url)
 
     with col3:
         st.text(recommended_movie_names[2])
         st.image(recommended_movie_posters[2])
+        url=recommended_movie_homeage[2]
+        if pd.isna(url):
+            st.markdown("Movie Link is not available")
+        else:
+            st.markdown("Movie Link [link](%s)" % url)
     with col4:
         st.text(recommended_movie_names[3])
         st.image(recommended_movie_posters[3])
+        url=recommended_movie_homeage[3]
+        if pd.isna(url):
+            st.markdown("Movie Link is not available")
+        else:
+            st.markdown("Movie Link [link](%s)" % url)
     with col5:
         st.text(recommended_movie_names[4])
         st.image(recommended_movie_posters[4])
+        url=recommended_movie_homeage[4]
+        if pd.isna(url):
+            st.markdown("Movie Link is not available")
+        else:
+            st.markdown("Movie Link [link](%s)" % url)
 
 url = "https://drive.google.com/file/d/1k1-NGrQOzMWZbmmkS4FKRSuV4_8-aJM3/view?usp=sharing"
 st.write("My Resume :- [link](%s)" % url)
